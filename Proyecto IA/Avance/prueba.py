b0d76197ac6580d6cde4c0ad5a3742641fe31d21
Celina <cmadrigalm19@gmail.com>
2023-10-10 14:55:08 -0600

    Tema y Avance

---
+++ Proyecto IA/Avance/prueba.py
@@ -0,0 +1,30 @@
+import openai
+from googletrans import Translator
+
+# Configurar clave de API de OpenAI
+openai.api_key = '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'
+
+# Interactuar con la API de ChatGPT
+def obtener_respuesta(prompt1):
+    response = openai.Completion.create(
+        engine="text-davinci-003",  #motor
+        prompt=prompt1,
+        max_tokens=2048  
+    )
+    return response.choices[0].text
+
+# Usar la función para obtener respuestas
+print()
+historia = input("Dame la sinopsis de tu historia: ")
+print()
+prompt1 = "Crea una historia de " + historia
+respuestaEs = obtener_respuesta(prompt1)
+print("Historia en español")
+print(respuestaEs+"\n")
+
+translator = Translator()
+
+respuestaEn = translator.translate(respuestaEs, src="es", dest="en")
+print("Historia en inglés\n")
+print(respuestaEn.text+"\n")
+
